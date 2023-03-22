0b9b3badc39946f116b907629478ad41331830eb
faraahahaha <alfa@jkt1.ebdesk.com>
2023-03-22 23:38:16 +0700

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -10,22 +10,22 @@ if __name__ == '__main__':
     
     start_time = time.time()
     
-    url = 'https://dumps.wikimedia.org/idwiki/latest/idwiki-latest-pages-articles.xml.bz2'
-    r = requests.get(url, stream=True)
-    path = 'data/idwiki-latest-pages-articles.xml.bz2'
-    print("Downloading dataset from idwiki latest pages articles")
-    with open(path, 'wb') as f:
-        total_length = int(r.headers.get('content-length'))
-        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
-            if chunk:
-                f.write(chunk)
-                f.flush()
-    print("Download complete")
+    # url = 'https://dumps.wikimedia.org/idwiki/latest/idwiki-latest-pages-articles.xml.bz2'
+    # r = requests.get(url, stream=True)
+    # path = 'data/idwiki-latest-pages-articles.xml.bz2'
+    # print("Downloading dataset from idwiki latest pages articles")
+    # with open(path, 'wb') as f:
+    #     total_length = int(r.headers.get('content-length'))
+    #     for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+    #         if chunk:
+    #             f.write(chunk)
+    #             f.flush()
+    # print("Download complete")
     
     print('Streaming wiki...')
     id_wiki = gensim.corpora.WikiCorpus(
         'data/idwiki-latest-pages-articles.xml.bz2',
-        dictionary={}, lower=True
+        dictionary={}, lower=False
     )
     
     article_count = 0
