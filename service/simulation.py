74246cd7e4a4eb2646e05e887b6daeb536f5e876
faraahahaha <48936892+Alfaalputra@users.noreply.github.com>
2023-04-07 09:45:36 +0700

    Create simulation.py

---
+++ service/simulation.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import streamlit as st
+
+path_this = os.path.dirname(os.path.abspath(__file__))
+root_path = os.path.abspath(os.path.join(path_this, ".."))
+src_path = os.path.join(root_path, "src")
+sys.path.append(src_path)
+
+from inference import Inference
+
+
+st.header("Word Segmenttation")
+inf = Inference()
+text = st.text_area('Input here')
+
+process = st.button("Process")
+if process:
+    result = {}
+    segment = inf.segment(text)
+
+    result["text"] = text
+    result["result"] = segment
+    st.write(result)
